ad982884392f4d0a1f29643ccac95b67ad2848fe
Sebastian Schulz <16666034+seb-schulz@users.noreply.github.com>
2026-04-01 09:56:04 +0000

    Fix test: use 'beans version' command instead of '--version' flag

--- test/setup-beans/setup-beans.sh
+++ test/setup-beans/setup-beans.sh
@@ -11,7 +11,7 @@ source dev-container-features-test-lib
 # Feature-specific tests
 # The 'check' command comes from the dev-container-features-test-lib.
 check "beans is installed" which beans
-check "beans is executable" beans --version
+check "beans is executable" beans version
 
 # Report results
 # If any of the checks above exited with a non-zero exit code, the test will fail.
